fe90a5f5cfbd59ee705430b551605a711b62715f
Allen P <reeldba@users.noreply.github.com>
2017-10-27 17:05:24 -0400

    working copy

--- bin/hab_control.py
+++ bin/hab_control.py
@@ -17,14 +17,26 @@ def read_sensor(sensor, sensor_pin):
     """
 
     import Adafruit_DHT
+    import time
 
-    LOGGER.debug('howdy')
+    LOGGER.debug('reading Adafruit_DHT sensor')
 
-    try:
-        humidity,temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
+    attempt=0;
+    while attempt < 9:
+        LOGGER.debug('attempt %d',attempt)
+        try:
+            humidity,temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
 
-    except Exception as e:
-        print str(e)
+            # attempt to validate the readings
+            if (humidity > 0 and humidity < 100) and (temperature > 0 and temperature < 100 ):
+                break
+
+        except Exception as e:
+            print str(e)
+
+        attempt = attempt + 1
+        time.sleep(1)
+        
 
     return humidity, temperature
 
@@ -60,11 +72,15 @@ def main():
     import sys
     import logging
     from get_config_file import get_config_json
+    import subprocess
+
     #from heater_control import heater_control
 
     global LOGGER
 
     cfg = get_config_json(sys.argv[1])
+
+    CODESEND=cfg['hab_control'].get('codesend')
 
     LOGGER=setup_logging(cfg['hab_control'].get('log_file'), logging.DEBUG)
 
@@ -82,6 +98,8 @@ def main():
 
     heater_list = sorted(remotes_cfg['remotes'], key=lambda k: k['watts'])
 
+    LOGGER.info(heater_list[0].get('label'))
+
     humidity,temperature = read_sensor(SENSOR, SENSOR_PIN)
     
     LOGGER.debug("RH=%6.2f%%, T=%6.2fC",humidity, temperature)
@@ -90,23 +108,34 @@ def main():
     if temperature > UPPER_ALERT_TEMP:
         LOGGER.debug("T exceeds %6.2fC upper alert limit", UPPER_ALERT_TEMP)
         #print "temp is {0:6.2} C above target and {1:6.2} C above upper limit".format(temperature-TARGET_TEMP,temperature-UPPER_ALERT_TEMP)
+        subprocess.call([CODESEND, heater_list[0].get('off'), "-l", heater_list[0].get('off-pulse')])
+        subprocess.call([CODESEND, heater_list[1].get('off'), "-l", heater_list[1].get('off-pulse')])
 
     if temperature > UPPER_CONTROL_TEMP and temperature <= UPPER_ALERT_TEMP:
         LOGGER.debug("T above %6.2fC uppert alert limit", UPPER_CONTROL_TEMP)
+        subprocess.call([CODESEND, heater_list[0].get('off'), "-l", heater_list[0].get('off-pulse')])
+        subprocess.call([CODESEND, heater_list[1].get('off'), "-l", heater_list[1].get('off-pulse')])
 
     if temperature >= TARGET_TEMP and temperature <= UPPER_CONTROL_TEMP:
         LOGGER.debug("T nominal. Heaters off")
         LOGGER.debug("Disable %s". heater_list[0].get('label'))
+        subprocess.call([CODESEND, heater_list[0].get('off'), "-l", heater_list[0].get('off-pulse')])
+        subprocess.call([CODESEND, heater_list[1].get('off'), "-l", heater_list[1].get('off-pulse')])
 
     if temperature < TARGET_TEMP and temperature >= LOWER_CONTROL_TEMP:
         LOGGER.debug("T below target. Enable lowest power heat")
         LOGGER.debug("Enable %s". heater_list[0].get('label'))
         LOGGER.debug("Disable %s". heater_list[1].get('label'))
+        subprocess.call([CODESEND, heater_list[0].get('on'), "-l", heater_list[0].get('on-pulse')])
+        subprocess.call([CODESEND, heater_list[1].get('off'), "-l", heater_list[1].get('off-pulse')])
 
     if temperature < LOWER_CONTROL_TEMP and temperature > LOWER_ALERT_TEMP:
         LOGGER.debug("T below target. Enable all heaters")
         LOGGER.debug("Enable %s". heater_list[0].get('label'))
         LOGGER.debug("Enable %s". heater_list[1].get('label'))
+        subprocess.call([CODESEND, heater_list[0].get('on'), "-l", heater_list[0].get('on-pulse')])
+        subprocess.call([CODESEND, heater_list[1].get('on'), "-l", heater_list[1].get('on-pulse')])
+
 
     if temperature <= LOWER_ALERT_TEMP:
         LOGGER.debug("T below lower alert. heaters not keeping up")
